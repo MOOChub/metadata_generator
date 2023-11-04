2f2da9316f64f750886cf500d2853b3c695379da
Max.Thomas <max.thomas@hpi.de>
2023-11-04 23:34:22 +0100

    Cleaned java script: removed var and set to const + removed unnecessary, out-commented lines. Adjusted name of buttons in js and html to fit the documentation.txt

--- static/script.js
+++ static/script.js
@@ -4,19 +4,19 @@ async function create_category_selection (framework, level, value){
         document.getElementById("a1").remove();
     }
 
-    if(framework != "def"){
+    if(framework !== "def"){
 
-        var config = await get_config_processor(framework);
+        const config = await get_config_processor(framework);
 
         remove_unneeded_selections(level);
 
-        var div_id = framework + "-level" + level + "-container";
+        const div_id = framework + "-level" + level + "-container";
 
-        var div = document.createElement('div');
+        const div = document.createElement('div');
         div.id = div_id;
         div.className = "category_selection";
 
-        var dropdown = document.createElement('select');
+        const dropdown = document.createElement('select');
         dropdown.id = framework + "-level" + level;
         dropdown.name = framework + "-level" + level;
         dropdown.className = "category_dropdown";
@@ -25,7 +25,7 @@ async function create_category_selection (framework, level, value){
 
         div.appendChild(dropdown);
 
-        var divF = document.getElementById("SelectBox");
+        const divF = document.getElementById("SelectBox");
         divF.appendChild(div);
         dropdown.onchange = function (){
             if(level < config["NUMBER_OF_LEVELS"]){
@@ -37,33 +37,31 @@ async function create_category_selection (framework, level, value){
 
 function build_dropdown(id_dropdown, framework, level, value){
 
-    //return new Promise(function (resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    var def_opt = document.createElement('option');
-                    def_opt.text = " -- Select a value -- ";
-                    def_opt.value = "def";
-                    document.getElementById(id_dropdown).add(def_opt);
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const def_opt = document.createElement('option');
+                def_opt.text = " -- Select a value -- ";
+                def_opt.value = "def";
+                document.getElementById(id_dropdown).add(def_opt);
 
-                    var fields = JSON.parse(xhr.responseText);
+                const fields = JSON.parse(xhr.responseText);
 
-                    fields.forEach(function (entry) {
-                        add_option(id_dropdown, entry);
-                    });
-                } else {
-                    console.error("Error loading options: " + xhr.status);
-                }
+                fields.forEach(function (entry) {
+                    add_option(id_dropdown, entry);
+                });
+            } else {
+                console.error("Error loading options: " + xhr.status);
             }
-        };
-        xhr.open("GET", "load_fields?framework=" + encodeURIComponent(framework) + "&level=" + encodeURIComponent(level) + "&value=" + encodeURIComponent(value), true);
-        xhr.send();
-    //});
+        }
+    };
+    xhr.open("GET", "load_fields?framework=" + encodeURIComponent(framework) + "&level=" + encodeURIComponent(level) + "&value=" + encodeURIComponent(value), true);
+    xhr.send();
 }
 
 function add_option(id_dropdown, value){
-    var option = document.createElement('option');
+    const option = document.createElement('option');
     option.text = value;
     option.value = value;
 
@@ -71,10 +69,10 @@ function add_option(id_dropdown, value){
 }
 
 function remove_unneeded_selections (level){
-    var all_divs = document.querySelectorAll("div.category_selection");
+    const all_divs = document.querySelectorAll("div.category_selection");
     if(all_divs){
         all_divs.forEach(function(one_selection){
-            var check_level = parseInt(one_selection.id.split("level")[1]);
+            const check_level = parseInt(one_selection.id.split("level")[1]);
             if (check_level >= level){
                 one_selection.remove();
             }
@@ -89,22 +87,22 @@ async function add_field() {
 }
 
 async function post_data(){
-    var framework = document.getElementById("select-framework").value;
+    const framework = document.getElementById("select-framework").value;
 
-    var level = await get_config_processor(framework);
-    var level = level["NUMBER_OF_LEVELS"];
+    const config = await get_config_processor(framework);
+    const level = config["NUMBER_OF_LEVELS"];
 
-    var field = document.getElementById(framework + "-level" + level).value;
-    var forgoing = document.getElementById(framework + "-level" + (level - 1)).value;
+    const field = document.getElementById(framework + "-level" + level).value;
+    const forgoing = document.getElementById(framework + "-level" + (level - 1)).value;
 
-    var value = {
+    const value = {
         "framework": framework,
         "field": field,
         "foregoing": forgoing,
     };
 
     return new Promise(function (resolve, reject){
-        var xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.open("POST", "/add_field", true);
         xhr.setRequestHeader("Content-Type", "application/json");
 
@@ -124,7 +122,7 @@ async function post_data(){
 }
 
 function remove_field(framework, field_to_remove, field_category) {
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', "/delete_field?framework=" + encodeURIComponent(framework) +
         "&value=" + encodeURIComponent(field_to_remove) + "&value_category=" +
         encodeURIComponent(field_category), true);
@@ -134,32 +132,31 @@ function remove_field(framework, field_to_remove, field_category) {
 }
 
 function show_all_selected_fields() {
-    var div = document.getElementById('list-selected-values-container');
-    var all_ols = div.querySelectorAll('ol');
+    const div = document.getElementById('list-selected-values-container');
+    const all_ols = div.querySelectorAll('ol');
     if(all_ols) {
         all_ols.forEach(function(ol){
            ol.remove();
         });
     }
 
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE){
             if (xhr.status === 200){
-                var data = JSON.parse(xhr.responseText);
-                var keys = Object.keys(data);
+                const data = JSON.parse(xhr.responseText);
+                const keys = Object.keys(data);
                 keys.sort();
                 keys.forEach(function (key) {
-                    var list = document.createElement('ol');
+                    const list = document.createElement('ol');
                     list.id = key;
-                    var list_title = document.createElement('h2');
+                    const list_title = document.createElement('h2');
                     list_title.textContent = key;
                     list.appendChild(list_title);
 
-                    var values = data[key];
+                    const values = data[key];
                     values.sort();
                     values.forEach(function(entry){
-                        //document.write(entry);
                         add_list_entry(list, entry);
                     });
                     div.appendChild(list);
@@ -174,12 +171,12 @@ function show_all_selected_fields() {
 
 function add_list_entry(list, value, forgoing_value) {
 
-    var list_entry = document.createElement('li');
-    var delete_button = document.createElement('button');
+    const list_entry = document.createElement('li');
+    const delete_button = document.createElement('button');
     delete_button.className = "DelButton";
     list_entry.className = "liEl";
     list_entry.textContent = value;
-    delete_button.textContent = "Delete field";
+    delete_button.textContent = "Delete Entry";
     delete_button.onclick = function () {
         remove_field(list.id, value, forgoing_value);
     };
@@ -189,11 +186,11 @@ function add_list_entry(list, value, forgoing_value) {
 }
 
 function write_json() {
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', '/write_json', true);
     xhr.send();
 
-    var link_for_download = document.createElement('a');
+    const link_for_download = document.createElement('a');
     link_for_download.id = "a1";
     link_for_download.textContent = "If download does not start automatically, click this link.";
     link_for_download.href = "http://localhost:5000/write_json";
@@ -216,7 +213,7 @@ function reset(){
     if (document.getElementById("a1") != null){
         document.getElementById("a1").remove();
     }
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', '/reset');
     xhr.send();
 }
@@ -224,12 +221,12 @@ function reset(){
 function get_config_processor(framework) {
 
     return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
 
-                    var config = JSON.parse(xhr.responseText);
+                    const config = JSON.parse(xhr.responseText);
                     resolve(config);
 
                 } else {
