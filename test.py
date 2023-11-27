fad0ffb2f40f243ed8715826ba5a7e745584000f
Max.Thomas <max.thomas@hpi.de>
2023-11-27 12:38:31 +0100

    - adjusted test.py to iterate through all frameworks in the new format -> test did not show any errors

--- test.py
+++ test.py
@@ -1,73 +1,40 @@
+import os
+
 import requests
 import pandas as pd
+import config_handler
+import frameworkprocessor
 
 error_log = []
 
 url = "http://localhost:5000/add_field"
 headers = {"Content-Type": "application/json"}
 
-frameworks = ["knowledge", "skill", "oefos"]
-names = ["ISCED-F", "ESCO 1.1.1", "oefos"]
-files = ["ESCO 1.1.1", "oefos"]
 
-for i in range(len(frameworks)):
-    framework = frameworks[i]
-    name = names[i]
-    if framework == frameworks[2]:
-        data_sep = ';'
-        file = files[1]
-    else:
-        data_sep = ','
-        file = files[0]
+for file in os.listdir("frameworks"):
+    path = os.path.join("frameworks", file)
+    data = pd.read_csv(path, sep=";", header=0)
 
-    path = "frameworks/" + file + ".csv"
+    framework = frameworkprocessor.FrameworkProcessor.remove_file_ending(file)
+    config = config_handler.get_config_processor_by_framework(framework)
 
-    data = pd.read_csv(path, sep=data_sep, dtype=str)
+    level = config.NUMBER_OF_LEVELS
 
-    if file == files[0]:
-        data = data[data['Level 0 preferred term'] == framework]
-        data = data[data['Level 3 preferred term'].notna()]
+    data = data[data["Level"] == level]
 
-        for index, row in data.iterrows():
-            data1 = row['Level 3 preferred term']
-            data2 = row['Level 2 preferred term']
+    for index, row in data.iterrows():
+        input_data = {
+            "framework": framework,
+            "value": row["Name"],
+            "forgoing": row["BroaderConcept"],
+        }
 
-            input_data = {
-                "framework": name,
-                "field": data1,
-                "foregoing": data2
-            }
-
-            try:
-                response = requests.post(url, json=input_data, headers=headers)
-                if response.status_code != 200:
-                    raise Exception()
-
-            except:
-                error_log.append(input_data)
-    else:
-        temp = data.copy()
-        data = data[data['Ebene'] == "4"]
-        temp = temp[temp['Ebene'] == "3"]
-
-        for index, row in data.iterrows():
-            data1 = row['Titel']
-            forgoing_code = row['Code'][:-2]
-            data2 = temp[temp['Code'] == forgoing_code]['Titel'].item()
-
-            input_data = {
-                "framework": name,
-                "field": data1,
-                "foregoing": data2
-            }
-
-            try:
-                response = requests.post(url, json=input_data, headers=headers)
-                if response.status_code != 200:
-                    raise Exception()
-
-            except:
-                error_log.append(input_data)
+        try:
+            response = requests.post(url, json=input_data, headers=headers)
+            if response.status_code != 200:
+                raise Exception()
+        except:
+            error_log.append(input_data)
 
 print(error_log)
 with open("error.log", 'w') as f:
